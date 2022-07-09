c020efd6bf28373a8c007c0a6d9b074a24e1ed74
Pedro Dias <pedro158a@gmail.com>
2022-07-09 11:45:57 +0100

    Define openapi object at startup and define properties in Dto

--- src/users/dto/user-create.dto.ts
+++ src/users/dto/user-create.dto.ts
@@ -1,18 +1,39 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserCreateDto {
+  @ApiProperty({
+    description: 'The user email.',
+    type: String,
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'The first name of the user.',
+    type: String,
+  })
   @IsNotEmpty()
   first_name: string;
 
+  @ApiProperty({
+    description: 'The user last name.',
+    type: String,
+  })
   @IsNotEmpty()
   last_name: string;
 
+  @ApiProperty({
+    description: 'The user username.',
+    type: String,
+  })
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    description: 'The user password.',
+    type: String,
+  })
   @IsNotEmpty()
   password: string;
 }
