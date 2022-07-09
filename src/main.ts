fb3317e18ee82652ba5b48d7d11792bc5e0f2e90
Pedro Dias <pedro158a@gmail.com>
2022-07-09 01:53:55 +0100

    Add basic security to api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(csurf());
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
