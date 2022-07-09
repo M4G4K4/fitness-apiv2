e56642c07b34dd12417589994346418f039bae98
Pedro Dias <pedro158a@gmail.com>
2022-07-09 11:52:52 +0100

    Add basic security to api (#4)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(csurf());
+
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Fitness app API')
@@ -15,6 +26,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
 
   await app.listen(3000);
 }
