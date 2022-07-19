72c3b412a51171edf7aa8f8b54859ba464823918
Pedro Dias <pedro158a@gmail.com>
2022-07-20 00:36:43 +0100

    Add TODO

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,5 +1,6 @@
 import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 
+
 export const config: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
@@ -11,3 +12,24 @@ export const config: TypeOrmModuleOptions = {
     migrations: [__dirname + '/migrations/*.ts'],
     synchronize: false,
 }
+
+//TODO: Check this
+/*
+Use this to class to use in:
+    imports: [
+        TypeOrmModule.forRoot(AppConstant.DB_CONFIG)
+
+export default class AppConstant {
+    static DB_CONFIG: TypeOrmModuleOptions = {
+        type: 'postgres', // HERE USE THE CONFIG.GET ou process.env.XXX
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'admin',
+        database: 'fitness',
+        autoLoadEntities: true,
+        migrations: [__dirname + '/migrations/*.ts'],
+        synchronize: false,
+    }
+}
+ */
